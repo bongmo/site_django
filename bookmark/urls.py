e90461c9541f7403a661afd5edbdc1be4649f717
Bong MoKim <bongmokim@Bongs-MacBook-Pro.local>
2017-09-19 19:58:58 +0900

    - Dragon slayers

---
+++ bookmark/urls.py
@@ -0,0 +1,9 @@
+#bookmark/urls.py
+
+from django.conf.urls import url
+from .views import BookmarkListView, BookmarkDetailView
+
+urlpatterns = [
+    url(r'^$', BookmarkListView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', BookmarkDetailView.as_view(), name='detail'),
+]
